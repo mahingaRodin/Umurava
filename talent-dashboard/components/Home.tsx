b4b5991785d6b6c318c8aab7a21236f871963871
P-Odiix <mahingarodin@gmail.com>
2025-01-23 08:42:01 +0200

    "added"

---
+++ talent-dashboard/components/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Dashboard from "./Dashboard/Dashboard"
+
+const Home = () => {
+  return (
+      <div className="overflow-hidden">
+          <Dashboard />
+    </div>
+  )
+}
+
+export default Home
