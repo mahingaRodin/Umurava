986155ad9755af4d592df0bbcef0b856d77b5bb7
P-Odiix <mahingarodin@gmail.com>
2025-01-25 20:11:02 +0200

    "updated"

--- talent-dashboard/app/page.tsx
+++ talent-dashboard/app/page.tsx
@@ -1,11 +1,9 @@
-import Home from "@/components/Home"
 import React from 'react'
 import Dashboard from "./dashboard/page"
 
 const HomePage = () => {
   return (
     <div>
-      {/* <Home/> */}
       <Dashboard />
     </div>
   )
