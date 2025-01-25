81f18f46d159874b042100d0865092def177fed4
P-Odiix <mahingarodin@gmail.com>
2025-01-25 20:37:31 +0200

    "updated"

--- talent-dashboard/app/challenges/page.tsx
+++ talent-dashboard/app/challenges/page.tsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Sidebar from "../../components/Dashboard/Sidebar";
+import { FaSearch } from "react-icons/fa";
 
 const Challenges = () => {
   return (
-    <div>
-      <Sidebar />
+    <div className="flex h-screen">
+      <div className="fixed h-screen w-[260px]">
+        <Sidebar />
+      </div>
+      <div className="flex-1 overflow-y-auto ml-[260px]">
+        <div className="flex-1 p-8">
+          {/* header */}
+          <div className="flex justify-between items-center mb-8">
+            <div className="relative">
+              <input
+                type="search"
+                placeholder="Search here..."
+                className="pl-10 pr-4 py-2 rounded-lg border w-[800px]"
+              />
+              <FaSearch className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
