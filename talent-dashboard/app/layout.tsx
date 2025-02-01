1d4dd5c6309534ce8f1a62d8a01baf8108d1fb2d
P-Odiix <mahingarodin@gmail.com>
2025-02-01 23:33:07 +0200

    "updated"

--- talent-dashboard/app/layout.tsx
+++ talent-dashboard/app/layout.tsx
@@ -18,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={font.className}
-      >
+      <body className={font.className}>
         {children}
       </body>
     </html>
--- talent-dashboard/app/page.tsx
+++ talent-dashboard/app/page.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import Dashboard from "./dashboard/page"
-import Home from "./home/page"
-
+import React from "react";
+import Home from "./home/page";
+import About from "./about/page";
+import Metrics from "./metrics/page";
+import Skills from "./skills/page";
+import ChallPage from "./challpage/page";
+import Section from "./section/page";
+import Testimonials from "./testimonials/page";
+import Started from "./started/page";
+import Career from "./career/page";
+import Footer from "./footer/page";
 const HomePage = () => {
   return (
     <div>
       <Home />
+      <Metrics />
+      <Skills />
+      <ChallPage />
+      <Section />
+      <Testimonials />
+      <Started />
+      <Career />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
